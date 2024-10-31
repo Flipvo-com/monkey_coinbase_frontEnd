0012b16ba3470614f0ed9d29257dd239acfeaa4e
Pblak <youceflahreche@outlook.fr>
2024-10-31 01:19:25 +0100

    :lipstick: fixing stuff

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,14 @@
 import './assets/main.css'
-
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-// import Echo from 'laravel-echo';
-// import Pusher from 'pusher-js';
-
 import 'vuetify/styles'
 import "@/assets/css/_icons.css"
 import {createVuetify} from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import {aliases, fa} from 'vuetify/iconsets/fa'
-
 import Particles from "vue3-particles";
-
 import Vue3Toastify, {type ToastContainerOptions} from 'vue3-toastify';
 import {VTreeview} from 'vuetify/labs/VTreeview'
 import {VDateInput} from 'vuetify/labs/VDateInput'
@@ -23,7 +17,8 @@ import validationRules from "@/plugins/validation/validationRules";
 import canGoToPlugin from './plugins/canGoTo';
 import isRole from '@/plugins/roles';
 
-
+// Keyboard.setResizeMode({ mode: 'none' });
+// Keyboard.setAccessoryBarVisible({ isVisible: false });
 // const VITE_APP_PUSHER_APP_KEY = import.meta.env.VITE_APP_PUSHER_APP_KEY
 // const VITE_APP_PUSHER_APP_CLUSTER = import.meta.env.VITE_APP_PUSHER_APP_CLUSTER
 
