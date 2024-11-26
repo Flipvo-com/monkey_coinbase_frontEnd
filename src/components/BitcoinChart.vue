a72129485fae31438520e5ca965f7c8e5d712aa1
Chris Nowlan <chrisnowlan321@gmail.com>
2024-11-25 20:55:28 -0500

    Getting styles working, DashboardView.vue

---
+++ src/components/BitcoinChart.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- Sparkline Chart -->
+  <v-sparkline
+      :fill="fill"
+      :gradient="selectedGradient"
+      :line-width="lineWidth"
+      :model-value="value"
+      :padding="padding"
+      :smooth="smooth"
+      auto-draw
+  ></v-sparkline>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const fill = ref(true);
+const lineWidth = ref(2);
+const padding = ref(8);
+const smooth = ref(true);
+
+const gradients = [
+  ["#222"],
+  ["#42b3f4"],
+  ["red", "orange", "yellow"],
+  ["purple", "violet"],
+  ["#00c6ff", "#F0F", "#FF0"],
+  ["#f72047", "#ffd200", "#1feaea"],
+];
+
+const selectedGradient = ref(gradients[4]);
+
+// Sample data
+const value = ref([0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]);
+</script>
+
+<style scoped>
+</style>
