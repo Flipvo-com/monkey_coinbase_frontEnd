7c8efe5f9e3ef8504943d94d319bfa256ffa762f
Chris Nowlan <chrisnowlan321@gmail.com>
2024-11-27 22:27:12 -0500

    Auto redirect if logged in

--- src/components/common/LogOutButton.vue
+++ src/components/common/LogOutButton.vue
@@ -1,8 +1,9 @@
 <template>
   <v-btn @click="attemptLogout" color="red">Logout</v-btn>
 </template>
+
 <script setup lang="ts">
-import {loginState} from "@/stats/loginState";
+import {loginState} from "@/stats/loginState.js"; // Import the loginState
 
 const {logout} = loginState(); // Call loginState() to get logout
 
